5069048b81a051015e4d773265d487770ea3b081
Desiree Desario <desireedesario@gmail.com>
2016-08-12 11:09:05 -0700

    created project with all npm packages and folders/files

---
+++ config/database.js
@@ -0,0 +1,17 @@
+var mongoose = require('mongoose');
+
+// Use different database URIs based on whether an env var exists.
+var dbUri = process.env.MONGOLAB_URI ||
+            'mongodb://localhost/' + process.env.LOCAL_DB;
+
+if (!process.env.MONGOLAB_URI) {
+  // check that MongoD is running...
+  require('net').connect(27017, 'localhost').on('error', function() {
+    console.log("PRESTIGE WORLDWIDE NEEDS INVESTMENTS FROM MONGOD!");
+    process.exit(0);
+  });
+}
+
+mongoose.connect(dbUri);
+
+module.exports = mongoose;
