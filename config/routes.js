c47aa094fdc1b3539468de2f313ffc045786f380
Taylor Britton <k.taylor.britton@gmail.com>
2016-08-13 17:41:21 -0700

    creates posts controller index and create actions and begins creating routes

--- config/routes.js
+++ config/routes.js
@@ -2,10 +2,23 @@ var express = require('express');
 var router  = new express.Router();
 var passport = require('passport')
 
+// require controllers
+var pagesController = require('../controllers/pages_controller')
+var postsController = require('../controllers/posts_controller')
+
+
 
 router.get('/', function(req, res){
   res.render('pages/home')
 })
+// dashboard path
+router.route('/dashboard')
+  .get(pagesController.dashboard);
+
+// API for posts
+router.route('/api/posts')
+  .get(postsController.index)
+  .post(postsController.create);
 
 //LINKEDIN OAUTH
 router.get('/auth/linkedin', passport.authenticate(
@@ -13,6 +26,5 @@ router.get('/auth/linkedin', passport.authenticate(
  // ,
  // { scope: ['profile', 'email'] }
 ));
-
 
 module.exports = router;
---
+++ controllers/posts_controller.js
@@ -0,0 +1,81 @@
+var Post = require('../models/Post');
+
+module.exports = {
+  index:   index,
+  create:  create
+  // ,
+  // update:  update,
+  // destroy: destroy
+}
+
+// return all posts from the db as json
+// middleware takes in a req and a res and needs a next to tell the app when to pass along to the next middleware in the chain
+function index(req, res, next) {
+  console.log("Bootsy's on a roll, bebe!!");
+
+  Post.find({}, function(err, posts) {
+    // express automatically has some error handling
+    // if there's an error, pass it on to the next error handler
+    if (err) next(err);
+
+    res.json(posts);
+  });
+};
+
+function create(req, res, next) {
+  console.log('Brand new Bootsy post, Bobble', req.body);
+
+  var newPost =   new Post();
+  newPost.company = req.body.company;
+  newPost.content = req.body.content;
+  newPost.interviewed = req.body.interviewed;
+  newPost.positive_exp = req.body.positive_exp;
+  newPost.bonus_tips = req.body.bonus_tips;
+  newPost.replies = req.body.replies;
+
+  newPost.save(function(err, savedPost) {
+    if (err) next(err);
+
+    res.json(savedPost);
+  });
+}
+
+// function update(req, res, next) {
+//   var id = req.params.id;
+//   console.log('Show bootsy that body, bebe!!!', id, req.body);
+//
+//   Post.findById(id, function(err, post) {
+//     if (err || !post) {
+//       next(err)
+//     } else {
+//       // set the new info if it exists in the request (if there are a ton of properties look up the programmatic way to do this)
+//       // if (req.body.task) post.task = req.body.task;
+//       // if (req.body.bootsyLevel) post.bootsyLevel = req.body.bootsyLevel;
+//       // post.completed = req.body.completed;
+//
+//       // We are only allowing users to check or uncheck completed box on the post page, so for this specific case, i can ignore the request data and just flip the boolean.
+//       // for a proper update, with actual changing data, see the code commented out above
+//       post.completed = !post.completed;
+//
+//       post.save(function(err, updatedPost) {
+//         if (err) next(err);
+//
+//         console.log('Yabba dabba doozy, baba - we changed it up!');
+//         res.json(updatedPost);
+//       })
+//     }
+//   })
+// }
+//
+// function destroy(req, res, next) {
+//   var id = req.params.id;
+//   console.log('Say toodaloo, post, Bootsy!!', id);
+//
+//   Post.remove({_id: id}, function(err) {
+//     // only triggers if there is a major problem; will not fail if trying to remove something that isn't there
+//     if (err) next(err);
+//
+//     // let us know if it's a successful delete
+//     res.json({msg: 'Just let that post chill, baby!'});
+//   });
+// }
