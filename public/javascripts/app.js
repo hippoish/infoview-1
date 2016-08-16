759dc197d44ae752b48529f5639738d81bb5ff4a
Desi Desario <desireedesario@gmail.com>
2016-08-16 11:24:04 -0700

    took out mock up and fixes bugs

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -151,18 +151,20 @@ $(document).ready(function() {
         // use previously defined fcn to create an html representation of the post
         var postHTML = createPostHTML(jsonPost);
         // append the html post to the DOM
-        $postsList.append(postHTML);
+        if (jsonPost.interviewed) {
+          $senseiPosts.append(postHTML);
+        } else {
+          $grasshopperPosts.append(postHTML);
+        }
       }
     )
   })
 
 // Attach event handlers through delegation.
  // When a selector is provided(as the second argument, i.e. ":checkbox" or ".remove-item"), the event handler is referred to as delegated. The handler is not called when the event occurs directly on the bound element, but only for descendants (inner elements) that match the selector.
- // $personalTodo.on("click", ":checkbox", updateHandler);
- // $bootsyTodo.on("click", ":checkbox", updateHandler);
- // $personalTodo.on("click", ".remove-item", deleteHandler);
-  $postsList.on("click", ".remove-post", deleteHandler);
-  $postsList.on("click", function() {
-    console.log("clicked me!!!! ouch!")
-  });
+ $senseiPosts.on("click", ":checkbox", updateHandler);
+ $grasshopperPosts.on("click", ":checkbox", updateHandler);
+ $senseiPosts.on("click", ".remove-post", deleteHandler);
+ $grasshopperPosts.on("click", ".remove-post", deleteHandler);
+
 });
